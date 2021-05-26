ae768dd5f77c97bb93213a53b34d954013a4d20a
unknown <John-Teology>
2021-05-26 09:59:06 +0800

    edit

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit/config.toml
 
 echo "\
 [general]\n\
@@ -10,4 +10,12 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+
+[theme]
+base="dark"
+primaryColor = '#69FFB3'
+backgroundColor = '#3037351F'
+secondaryBackgroundColor= '#f37b05'
+textColor = '#FFFFFF'
+font = "sans serif"
 " > ~/.streamlit/config.toml
