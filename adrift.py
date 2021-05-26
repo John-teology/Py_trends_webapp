87bdc065f100adb1c449db4f8c96a3bfd62f8fc8
unknown <John-Teology>
2021-05-26 15:24:16 +0800

    editt

--- adrift.py
+++ adrift.py
@@ -144,8 +144,10 @@ st.markdown("""
     background: #1FDE83;
 }
 .line_chart{
-    background: #0C1916; 
-    color:rgb(255, 255, 255);
+    background: #0C1916;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(0.5turn,#2E3734CC,#07221C);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(0.5turn,#2E3734CC,#07221C); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    color: rgb(255, 255, 255);
 }
 </style>
 """,unsafe_allow_html=True
