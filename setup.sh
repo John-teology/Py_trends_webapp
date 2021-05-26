97275f87a849c0ec774077bc451a1db6afeff09a
unknown <John-Teology>
2021-05-26 12:23:29 +0800

    edit

--- setup.sh
+++ setup.sh
@@ -5,9 +5,16 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
+echo "[theme] "\
+primaryColor = "#84a3a7"
+backgroundColor = "#EFEDE8"
+secondaryBackgroundColor = "#fafafa"
+textColor= "#424242"
+font = "sans serif"
+
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
+enableCORS=true\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
